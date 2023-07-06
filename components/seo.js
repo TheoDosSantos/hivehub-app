7c7204940eee88879e9e7bdfa524e36a2fcca55c
TheoDosSantos <theo@callbruno.com>
2023-07-06 20:14:11 +0200

    ✅ Update - Global - Adding favion & update .gitignore

--- components/seo.js
+++ components/seo.js
@@ -19,6 +19,7 @@ const SEO = ({ title, description, image }) => {
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
       {/* <!-- Favicon --> */}
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
