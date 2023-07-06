7023b27937951abd8028b4e922fda13a7217c366
TheoDosSantos <theo@callbruno.com>
2023-07-06 19:50:40 +0200

    ✨ Update - Global - Improve features about JWT and stay logged by using it

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext({
   isAuth: false,
@@ -19,6 +19,26 @@ export const UserProvider = ({ children }) => {
     setIsAuth(false);
     setUserInfo({});
   };
+
+  useEffect(() => {
+    const currentUser = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/users/log/current`,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log(data);
+        login(data);
+      } else {
+        logout();
+      }
+    };
+    currentUser();
+  }, []);
 
   return (
     <UserContext.Provider value={{ isAuth, userInfo, login, logout }}>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,21 @@
-import "@/styles/common/reset.scss";
+import { useEffect, useState, useContext } from "react";
+import { useRouter } from 'next/router';
+import { UserProvider, UserContext } from "context/userContext";
 
-import { UserProvider } from "context/userContext";
+import "@/styles/common/reset.scss";
+import "@/styles/common/calendar.scss";
 
 export default function App({ Component, pageProps }) {
+  
+  const router = useRouter();
+  const { isAuth } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.push("/");
+    }
+  }, []);
+
   return (
     <UserProvider>
       <Component {...pageProps} />
